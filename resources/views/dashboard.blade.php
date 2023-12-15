4709b5658b079574273340338236d1c98fa3634a
cp_carlo <carlo.dinopol@checkproof.com>
2023-12-15 15:22:21 +0100

    reverted assets

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Laravel Schedule Control</title>
 
-        <link href="{{ asset('/vendor/schedule-control/bootstrap.min.css') }}" rel="stylesheet">
-        <link href="{{ asset('/vendor/schedule-control/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('/vendor/schedule-control/css/bootstrap.min.css') }}" rel="stylesheet">
+        <link href="{{ asset('/vendor/schedule-control/css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         <div class="container w-75 px-5">
@@ -101,7 +101,7 @@
             </div>
         </div>
 
-        <script src="{{ asset('/vendor/schedule-control/bootstrap.min.js') }}"></script>
-        <script src="{{ asset('/vendor/schedule-control/app.js') }}"></script>
+        <script src="{{ asset('/vendor/schedule-control/js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('/vendor/schedule-control/js/app.js') }}"></script>
     </body>
 </html>
