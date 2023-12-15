2bd2ffe18ac9d31ff12f1d3cb79ffc7546c1237d
cp_carlo <carlo.dinopol@checkproof.com>
2023-12-15 07:37:18 +0100

    loaded css and js within public assets

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,14 +1,12 @@
-
 <html lang="">
     <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-        <title></title>
+        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+        <title>Laravel Schedule Control</title>
     </head>
     <body>
         <div class="container">
             <div class="py-5">
-                <h1>Scheduler Control</h1>
+                <h1>Laravel Schedule Control</h1>
             </div>
             @if($configured)
                 <div class="row">
@@ -79,7 +77,10 @@
                         <li>extend <i>ControlledConsoleKernel</i> in your <i>\App\Console\Kernel</i>.</li>
                         <li> migrate: <i>php artisan migrate</i>.</li>
                     </ul>
+                </div>
             @endif
         </div>
+
+        <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
     </body>
 </html>
--- src/ScheduleControlServiceProvider.php
+++ src/ScheduleControlServiceProvider.php
@@ -14,18 +14,32 @@ class ScheduleControlServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        $this->load();
+        $this->publish();
+    }
+
+    private function load(): void
+    {
         // Migrations
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
         // Views
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'schedule-control');
 
-        // Publishes the configuration file
+        // Routes
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+    }
+
+    private function publish(): void
+    {
+        // Config
         $this->publishes([
             __DIR__ . '/../config/schedule-control.php' => config_path('schedule-control.php'),
         ], 'schedule-control-config');
 
-        // Routes
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        // Assets
+        $this->publishes([
+            __DIR__.'/../public' => public_path('vendor/schedule-control'),
+        ], 'public');
     }
 }
